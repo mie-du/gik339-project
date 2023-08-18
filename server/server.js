d226f99bf76f5c24b0874beb748ff25d5b3ea9c8
Mikaela Hedberg <mie@du.se>
2023-08-18 12:46:50 +0200

    comments

--- server/server.js
+++ server/server.js
@@ -1,31 +1,46 @@
+/* Importera npm-paket sqlite3 med hjälp av require() och lagrar i variabeln sqlite */
 const sqlite = require('sqlite3').verbose();
+/* Skapar ny koppling till databas-fil som skapades tidigare. */
 const db = new sqlite.Database('./gik339.db');
 
+/* Importerar npm-paket express och lagrar i variabeln express */
 const express = require('express');
+/* Skapar server med hjälp av express */
 const server = express();
 
+/* Sätter konfiguration på servern */
 server
+  /* Data ska kommuniceras i JSON-format */
   .use(express.json())
+  /* Sättet som data ska kodas och avkodas på */
   .use(express.urlencoded({ extended: false }))
   .use((req, res, next) => {
+    /* Headers för alla förfrågningar. Hanterar regler för CORS (vilka klienter som får anropa vår server och hur.) */
     res.header('Access-Controll-Allow-Origin', '*');
     res.header('Access-Controll-Allow-Headers', '*');
     res.header('Access-Controll-Allow-Methods', '*');
-
+    /* Säger åt servern att fortsätta processa förfrågan */
     next();
   });
 
+/* Startar servern på port 3000 */
 server.listen(3000, () => {
+  /* Meddelande för feedback att servern körs */
   console.log('Server running on http://localhost:3000');
 });
 
+/* Hantering av GET-requests till endpointen /users */
 server.get('/users', (req, res) => {
+  /* sql-query för att hämta alla users ur databasen. */
   const sql = 'SELECT * FROM users';
-
+  /* Anrop till db-objektets funktion .all som används till att hämta upp rader ur en tabell */
   db.all(sql, (err, rows) => {
+    /* Callbackfunktionen har parametern err för att lagra eventuella fel */
     if (err) {
+      /* Om det finns något i det objektet skickar vi ett svar tillbaka att något gick fel (status 500) och info om vad som gick fel (innehållet i objektet err) */
       res.status(500).send(err);
     } else {
+      /* Annars, om allt gick bra, skickar vi de rader som hämtades upp.  */
       res.send(rows);
     }
   });
