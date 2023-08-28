a9ae26d4973d09090f44e46d72caee1af15f83d4
Mikaela Hedberg <mie@du.se>
2023-08-28 13:24:53 +0200

    put, getone, delete

--- server/server.js
+++ server/server.js
@@ -31,6 +31,20 @@ server.get('/users', (req, res) => {
   });
 });
 
+server.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM users WHERE id=${id}`;
+
+  db.all(sql, (err, rows) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(rows[0]);
+    }
+  });
+});
+
 server.post('/users', (req, res) => {
   const user = req.body;
   const sql = `INSERT INTO users(firstName, lastName, username, color) VALUES (?,?,?,?)`;
@@ -41,6 +55,50 @@ server.post('/users', (req, res) => {
       res.status(500).send(err);
     } else {
       res.send('Användaren sparades');
+    }
+  });
+});
+
+server.put('/users', (req, res) => {
+  const bodyData = req.body;
+
+  const id = bodyData.id;
+  const user = {
+    firstName: bodyData.firstName,
+    lastName: bodyData.lastName,
+    username: bodyData.username,
+    color: bodyData.color
+  };
+
+  let updateString = '';
+  const columnsArray = Object.keys(user);
+  columnsArray.forEach((column, i) => {
+    updateString += `${column}="${user[column]}"`;
+    if (i !== columnsArray.length - 1) updateString += ',';
+  });
+  const sql = `UPDATE users SET ${updateString} WHERE id=${id}`;
+
+  db.run(sql, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send('Användaren uppdaterades');
+    }
+  });
+  //UPDATE users SET firstName="Mikaela",lastName="Hedberg" WHERE id=1
+});
+
+server.delete('/users/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `DELETE FROM users WHERE id = ${id}`;
+
+  db.run(sql, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send('Användaren borttagen');
     }
   });
 });
